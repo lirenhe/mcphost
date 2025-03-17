7ed556f7d4495b4beaf108d39898249bb2d25617
FAREAST\renhel <renhe.li@hotmail.com>
2025-03-17 14:26:34 +0800

    update

--- mcp/src/decorators.ts
+++ mcp/src/decorators.ts
@@ -1,4 +1,4 @@
-import { DecoratorContext, Operation, Program } from "@typespec/compiler";
+import { DecoratorContext, Namespace, Operation, Program } from "@typespec/compiler";
 import { StateKeys, reportDiagnostic } from "./lib.js";
 
 export const namespace = "Mcp";
@@ -33,6 +33,13 @@ export function getAlternateName(program: Program, target: Operation): string | 
 }
 
 
+export interface McpServiceOptions {
+  readonly title?: string;
+  readonly version?: string;
+  readonly description?: string;
+}
+
+
 /**
  * __Example implementation of the `@alternateName` decorator.__
  *
@@ -40,15 +47,15 @@ export function getAlternateName(program: Program, target: Operation): string | 
  * @param target Decorator target. Must be an operation.
  * @param name Alternate name.
  */
-export function $mcpServer(context: DecoratorContext, target: unknown, name: string, description?: string) {
-  if (name === "banned") {
+export function $mcpServer(context: DecoratorContext, target: Namespace, options: McpServiceOptions) {
+  if (options.title === "banned") {
     reportDiagnostic(context.program, {
       code: "banned-alternate-name",
       target: context.getArgumentTarget(0)!,
-      format: { name },
+      format: { "name": options.title },
     });
   }
-  context.program.stateMap(StateKeys.alternateName).set(target as any, name);
+  context.program.stateMap(StateKeys.alternateName).set(target as any, options.title);
 }
 
 /**
@@ -58,6 +65,6 @@ export function $mcpServer(context: DecoratorContext, target: unknown, name: str
  * @param target Decorator target. Must be an operation.
  * @returns Altenate name if provided on the given operation or undefined
  */
-export function mcpServer(program: Program, target: unknown): string | undefined {
+export function mcpServer(program: Program, target: Namespace): string | undefined {
   return program.stateMap(StateKeys.alternateName).get(target as any);
 }
